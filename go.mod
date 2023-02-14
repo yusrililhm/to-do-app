b76f77a4609be29a76bb270e8216a2dcc69f5264
yusrililhm <yusrililham62@gmail.com>
2023-02-14 13:21:16 +0000

    Create To Do List App with ReactJS, Go, MongoDB

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yusrililhm/to-do-app
+
+go 1.20
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.js"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      "Poppins": "Poppins",
+      "sans-serif": "sans-serif"
+    }
+  },
+  plugins: [],
+}
