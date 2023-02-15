aab3107436c079cdcaf4cdc9d6d3bbb24f34bb91
yusrililhm <yusrililham62@gmail.com>
2023-02-15 16:13:09 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/yusrililhm/to-do-app/server/routes"
@@ -8,5 +9,6 @@ import (
 
 func main() {
 	routes.Routes()
+	fmt.Println("Server Is Running ON PORT 8000")
 	http.ListenAndServe(":8000", nil)
 }
