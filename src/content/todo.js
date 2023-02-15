6c98c9e1e993fe4f95af2c5a3b60d7e85418b9a0
yusrililhm <yusrililham62@gmail.com>
2023-02-15 13:36:59 +0000

    Update todo

--- src/content/todo.js
+++ src/content/todo.js
@@ -2,10 +2,12 @@ import '../assets/css/index.css'
 
 function Todo() {
     return (
-        <section className='font-Poppins'>
-            <form method='POST' action='/Add'>
-                <input type='text' name='todo' id='todo' placeholder='Type your plan here' />
-            </form>
+        <section className='font-Poppins h-screen'>
+            <div className='flex justify-center items-center'>
+                <form method='POST' action='/Add' className='flex justify-center items-center mt-20 bg-white shadow-lg py-2 px-4 rounded-full w-3/5'>
+                    <input type='text' name='todo' id='todo' placeholder='Type your plan here' className='w-full'/>
+                </form>
+            </div>
         </section>
     );
 }
